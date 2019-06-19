f2418a127d4a62da03895e3ea0c615271ea7a2e2
Shakiba <star.77279@yahoo.com>
2019-06-19 21:46:44 +0430

    ServerSender was completed. It's been improved. finally was removed and try with resourcs is added

--- Project/src/Download/ServerSender.java
+++ Project/src/Download/ServerSender.java
@@ -1,5 +1,66 @@
 package Download;
 
-public class ServerSender {
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+
+
+public class ServerSender extends Thread {
+	public final static int SOCKET_PORT = 4444;
+	public String FILE_TO_SEND;
+	
+	
+
+	public ServerSender(String name) {
+		super();
+		FILE_TO_SEND = name;
+	}
+
+	
+
+
+	public String getFILE_TO_SEND() {
+		return FILE_TO_SEND ;
+	}
+
+
+
+
+	public void run() {
+		
+	    try (ServerSocket servsock = new ServerSocket(SOCKET_PORT)){
+	    	while (true) {
+	    		System.out.println("Waiting...");
+	    		File myFile = new File (FILE_TO_SEND);
+	    		try (Socket sock = servsock.accept();
+	    				FileInputStream fis = new FileInputStream(myFile);
+	    				BufferedInputStream bis = new BufferedInputStream(fis);
+	    				OutputStream os = sock.getOutputStream();){
+	    			
+	    			
+	    			System.out.println("Accepted connection : " + sock);
+	    			byte [] mybytearray  = new byte [(int)myFile.length()];
+	    			bis.read(mybytearray,0,mybytearray.length);
+	    			System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+	    			os.write(mybytearray,0,mybytearray.length);
+	    			os.flush();
+	    			System.out.println("Done.");
+	    			
+	    			
+	    		} catch (IOException ex) {	
+	    			System.out.println(ex.getMessage()+": An Inbound Connection Was Not Resolved");
+	    		}
+	      
+	    	}
+	    } catch (IOException e) {
+	    	System.out.println("We are in Server and it's an io exception");
+			e.printStackTrace();
+		}
+	}
+	
 }
